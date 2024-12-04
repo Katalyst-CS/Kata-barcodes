16f1b300c275edf39799e904a6f8ca4a750dea8b
Marck Carrion Guzman <mcarrion@kcramsolutions.com>
2024-12-04 11:54:09 +0100

    feat(domain): creada respuesta de error

--- src/domain/outbound/error.rs
+++ src/domain/outbound/error.rs
@@ -1,4 +1,28 @@
+use serde::{Deserialize, Serialize};
 
+use crate::shared::commons::create_str;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub enum ResponseStatus {
+  OK,
+  KO,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 pub struct ErrorResponseDto {
-  // Respuesta de error para la api
+  pub message: Option<String>,
+  pub code: Option<u32>,
+  pub status: Option<ResponseStatus>
+}
+
+
+impl ErrorResponseDto {
+  pub fn new(message: &str, code: u32, status: ResponseStatus) -> Self
+  {
+    ErrorResponseDto {
+      message: Some(create_str(&message)),
+      code: Some(code),
+      status: Some(status)
+    }
+  }
 }
--- src/domain/outbound/mod.rs
+++ src/domain/outbound/mod.rs
@@ -1,1 +1,2 @@
 pub mod generation_response;
+pub mod error;
