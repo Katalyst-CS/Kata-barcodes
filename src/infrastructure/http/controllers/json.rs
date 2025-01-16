b5031f8527f891af600c36b25b316f2d6624c43d
Marck Carrion Guzman <mcarrion@kcramsolutions.com>
2025-01-16 12:50:45 +0100

    feat(infra/http): agregado generador de codigo de barras

--- src/infrastructure/http/controllers/json.rs
+++ src/infrastructure/http/controllers/json.rs
@@ -1,10 +1,14 @@
+use crate::core::generators::barcodes::proxy::BarcodeGeneratorProxy;
 use crate::domain::inbound::generation::GenerationRequestDto;
 use crate::domain::outbound::error::ErrorResponseDto;
-use crate::shared::crypto::check_sign;
+use crate::domain::outbound::generation::GenerationResponseDto;
+use crate::shared::checkers::{check_barcode, check_img};
+use crate::shared::crypto::{check_sign, encode_64, generate_sign};
 use log::{info, warn};
 use salvo::prelude::Json;
 use salvo::{handler, http::StatusCode, Request, Response};
 
+// TODO: Check signs
 #[handler]
 pub async fn json_handler(req: &mut Request, res: &mut Response) {
     let body_result: Result<GenerationRequestDto, salvo::http::ParseError> =
@@ -57,12 +61,49 @@ pub async fn json_handler(req: &mut Request, res: &mut Response) {
             return;
         }
     };
+    let height: u32 = match body.height {
+        Some(i) => {
+            info!("Body.height: OK");
+            i
+        },
+        None => {
+            warn!("No height in body");
+            let response = raise_error_valid("height".to_string());
+            res.status_code(StatusCode::BAD_REQUEST)
+                .render(Json(response));
+            return;
+        }
+    };
     let sign_str = format!("{}.{}", data, image);
     info!("String chain created {}", sign_str);
     let check = check_sign(sign, sign_str);
     info!("Signature: {}", check.unwrap());
-
-    res.render(Json(vec!["hola", "world"]));
+    if !check_barcode(barcode_option.clone().unwrap()) {
+        let response = raise_error_valid("code".to_string());
+        res.status_code(StatusCode::BAD_REQUEST)
+            .render(Json(response));
+        return;
+    }
+    if !check_img(image.clone()) {
+        let response = raise_error_valid("image".to_string());
+        res.status_code(StatusCode::BAD_REQUEST)
+            .render(Json(response));
+        return;
+    }
+    let generator = BarcodeGeneratorProxy::new(&barcode_option.clone().unwrap());
+    let out_img_result = generator.generate(data.as_str(), height, image.as_str());
+    let out_img = match out_img_result {
+        Err(e) => {
+            res.status_code(StatusCode::INTERNAL_SERVER_ERROR)
+                .render(Json(ErrorResponseDto::new(&e, 0x1005)));
+            return;
+        },
+        Ok(bytes) => bytes
+    };
+    let out_img_str = encode_64(out_img);
+    let out_sign = generate_sign(out_img_str.clone()).unwrap();
+    let response = GenerationResponseDto::new(&out_img_str, out_sign.as_str());
+    res.render(Json(response));
 }
 
 fn raise_error_valid(field: String) -> ErrorResponseDto {
